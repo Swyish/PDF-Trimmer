cfbd8c604326409b5657c83a29010f0fead2ca3d
Swyish <Swyish@live.com>
2013-10-18 14:13:19 -0400

    Adding a test class for pdftrimmerservice

---
+++ PDF-Trimmer.Services.Tests/PDFTrimmerServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PDFTrimmer.Services;
+
+namespace PDF_Trimmer.Services.Tests
+{
+    [TestClass]
+    public class PDFTrimmerServiceTests
+    {
+        private ITrimmerService _trimmerService;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _trimmerService = new PDFTrimmerService();
+        }
+
+        [TestMethod]
+        public void TrimmerService_Exists()
+        {
+            Assert.IsNotNull(_trimmerService);
+        }
+    }
+}
